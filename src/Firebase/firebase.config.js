8ed61a22604016a0ec3fcee42a62171cdebd94cd
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-05-05 21:56:10 +0600

    hide firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD4i4XlXXYGwG8Q3hBLCitTheoZL8d_X_U",
-  authDomain: "kitchen-genius-9adb2.firebaseapp.com",
-  projectId: "kitchen-genius-9adb2",
-  storageBucket: "kitchen-genius-9adb2.appspot.com",
-  messagingSenderId: "231298222390",
-  appId: "1:231298222390:web:86e03f7fbeb38de568cb97"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId:  import.meta.env.VITE_projectId,
+  storageBucket:  import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId:  import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
